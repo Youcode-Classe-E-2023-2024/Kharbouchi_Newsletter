927d4c481284eaed5c783e62cfc04221650443aa
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-08 16:04:22 +0100

    forgot password

--- resources/views/Auth/forget_pas.blade.php
+++ resources/views/Auth/forget_pas.blade.php
@@ -22,45 +22,20 @@
 						<div class="img" style="background-image: url('{{asset('signin/images/bg-1.jpg')}}');">
 			        </div>
 						<div class="login-wrap p-4 p-md-5">
-			      	<div class="d-flex">
-			      		<div class="w-100">
-			      			<h3 class="mb-4">Sign In</h3>
-			      		</div>
-								<div class="w-100">
-									<p class="social-media d-flex justify-content-end">
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
-									</p>
-								</div>
-			      	</div>
-					  @if(Session::has('error'))
-					  <div class="alert alert-danger" role="alert">
-						  {{ Session::get('error') }}
-					  </div>
-					  @endif
-							<form action="{{route('signin')}}" method="POST" class="signin-form">
-								@csrf
+                            <div class="d-flex justify-content-center">
+                                <div class="w-100 text-center">
+                                    <h3 class="mb-4">Forgot Password</h3>
+                                </div>
+                            </div>
+                            
+							<form action="" method="POST" class="signin-form">
+								
 			      		<div class="form-group mb-3">
 			      			<label class="label" for="name">Email</label>
 			      			<input type="email" name="email" class="form-control" placeholder="Email" required>
 			      		</div>
-		            <div class="form-group mb-3">
-		            	<label class="label" for="password">Password</label>
-		              <input type="password" name="password" class="form-control" placeholder="Password" required>
-		            </div>
 		            <div class="form-group">
-		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
-		            </div>
-		            <div class="form-group d-md-flex">
-		            	<div class="w-50 text-left">
-			            	<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
-									  <input type="checkbox" checked>
-									  <span class="checkmark"></span>
-										</label>
-									</div>
-									<div class="w-50 text-md-right">
-										<a href="forget_pas">Forgot Password</a>
-									</div>
+		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Send Email</button>
 		            </div>
 		          </form>
 		          
