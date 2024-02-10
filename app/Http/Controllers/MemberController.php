0d01e190ca49893b8a50c103f2596a0f22148727
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-10 22:35:05 +0100

    emails de members

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
+use App\Models\Member;
 
 class MemberController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|unique:members,email', 
+        ]);
+
+        $member = new Member();
+        $member->email = $request->email;
+        $member->subscribe = true; 
+        $member->save();
+
+        return back()->with('success', 'You have successfully subscribed!');
+    }
 }
+
