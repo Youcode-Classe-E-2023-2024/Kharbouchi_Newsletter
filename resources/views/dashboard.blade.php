f2e01fe5cc95f26c9468b7d6e3ea7ed1c37994e0
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-16 17:31:50 +0100

    modification

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -181,7 +181,7 @@
             <div>
                 <div class="brand-logo d-flex align-items-center justify-content-between">
                     <a href="#" class="text-nowrap logo-img">
-                        <img src="{{ asset('assets/images/logos/news-logo-dashboard.svg') }}" style="width: 80px; height: 80px;"  alt="logo" />
+                        <img src="{{ asset('assets/images/logos/news-logo-dashboard.svg') }}"  width="180" alt="" />
                     </a>
                     <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                         <i class="ti ti-x fs-8"></i>
