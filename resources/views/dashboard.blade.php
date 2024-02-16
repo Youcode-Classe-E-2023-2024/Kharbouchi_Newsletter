7dca5531d94c8ed9c998810a04a9d64505e7ea40
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-16 17:25:04 +0100

    front

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -181,7 +181,7 @@
             <div>
                 <div class="brand-logo d-flex align-items-center justify-content-between">
                     <a href="#" class="text-nowrap logo-img">
-                        <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" width="180" alt="" />
+                        <img src="{{ asset('assets/images/logos/news-logo-dashboard.svg') }}" style="width: 80px; height: 80px;"  alt="logo" />
                     </a>
                     <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                         <i class="ti ti-x fs-8"></i>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-3 fh5co_padding_menu">
-                <img src="{{asset('index/images/logo.png')}}" alt="img" class="fh5co_logo_width"/>
+                <img src="{{ asset('assets/images/logos/news-logo.svg') }}" alt="img" style="width: 80px; height: 80px;" class="fh5co_logo_width"/>
             </div>
             <div class="col-12 col-md-9 align-self-center fh5co_mediya_right">
                 <div class="text-center d-inline-block">
@@ -345,7 +345,6 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="js/owl.carousel.min.js"></script>
-<!--<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
         integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
         crossorigin="anonymous"></script>
